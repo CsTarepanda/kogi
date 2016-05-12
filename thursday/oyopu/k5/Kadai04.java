3890dae7d60a14acb6e0ecd7bc861b3c2ff82f9a
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-05-12 14:13:03 +0900

    fix kadai04

--- thursday/oyopu/k5/Kadai04.java
+++ thursday/oyopu/k5/Kadai04.java
@@ -1,11 +1,11 @@
 public class Kadai04{
 	public static void main(String[] args){
-    String str2 = "1404-1, Katakuramachi, Hachioji, Tokyo  192-0982,  JAPAN";
+    String str2 = "1404-1, Katakuramachi, Hachioji, Tokyo 192-0982, JAPAN";
     String result;
     System.out.println(str2);
     System.out.println(str2.replaceAll("achi", "cho"));
     System.out.println(str2.replaceAll("[a-n]", "*"));
-    System.out.println(str2.replaceAll("[tkmc]", "").replaceAll("a", "#"));
+    System.out.println(str2.replaceAll("a.", "#"));
     System.out.println(str2.replaceAll("[0-9]{4}", "????"));
 	}
 }
