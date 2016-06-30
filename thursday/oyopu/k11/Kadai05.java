a28eaf29d901f700eee7d909339b0440fde6530c
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-06-30 14:48:23 +0900

    整形

--- thursday/oyopu/k11/Kadai05.java
+++ thursday/oyopu/k11/Kadai05.java
@@ -1,14 +1,12 @@
 import java.util.*;
 public class Kadai05{
-  static {
-    String sen = "An eye for an eye, a tooth for a tooth.";
-    new TreeMap<String, Integer>(){{
-      System.out.println(sen);
-      for(String s: sen.toLowerCase().replaceAll("[,.]", "").split(" "))
-        if(putIfAbsent(s, 1) != null) put(s, get(s) + 1);
-      entrySet().forEach(e -> System.out.printf("%-10s: %d\n", e.getKey(), e.getValue()));
-    }};
-  }
+  static String sen = "An eye for an eye, a tooth for a tooth.";
+  static Object o = new TreeMap<String, Integer>(){{
+    System.out.println(sen);
+    for(String s: sen.toLowerCase().replaceAll("[,.]", "").split(" "))
+      if(putIfAbsent(s, 1) != null) put(s, get(s) + 1);
+    entrySet().forEach(e -> System.out.printf("%-10s: %d\n", e.getKey(), e.getValue()));
+  }};
 
-	public static void main(String[] args){}
+  public static void main(String[] args){}
 }
