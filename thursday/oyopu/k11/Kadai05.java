6341417d95468582aac5a48db4eab182855dad6e
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-06-30 13:52:15 +0900

    より短く

--- thursday/oyopu/k11/Kadai05.java
+++ thursday/oyopu/k11/Kadai05.java
@@ -3,13 +3,9 @@ public class Kadai05{
 	public static void main(String[] args){
     String sen = "An eye for an eye, a tooth for a tooth.";
     System.out.println(sen);
-    Map<String, ArrayList<String>> map = new TreeMap<String, ArrayList<String>>();
-    Arrays.asList(sen.toLowerCase().replaceAll("[,.]", "").split(" ")).forEach(s -> {
-      List<String> target = map.get(s);
-      if(target == null) map.put(s, new ArrayList<String>());
-      target = map.get(s);
-      target.add(s);
-    });
-    map.entrySet().forEach(e -> System.out.printf("%-10s: %d\n", e.getKey(), e.getValue().size()));
+    Map<String, Integer> map = new TreeMap<String, Integer>();
+    Arrays.asList(sen.toLowerCase().replaceAll("[,.]", "").split(" ")).forEach(s -> 
+      map.put(s, map.get(s) == null ? 1 : map.get(s) + 1));
+    map.entrySet().forEach(e -> System.out.printf("%-10s: %d\n", e.getKey(), e.getValue()));
 	}
 }
